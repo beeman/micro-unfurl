713f7c64084dd6e1f6173d75e4cd431f2a9cca28
Jack Tuck <jack-tuck@hotmail.com>
2018-06-22 00:58:26 +0100

    Added additional error handling and send json content type

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const { router, get } = require('microrouter')
+const { send } = require('micro')
 const unfurl = require('unfurl.js')
 
+const isUrl = /https?:\/\//
+
 const rootRoute = async (req, res) => {
-  if (req.query['url']) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    res.end(JSON.stringify(await unfurl(req.query['url'])));
-  } else {
-    res.end('Please submit parameter ?url=X');   
+  const url = req.query['url']
+
+  if (!url) {
+    return send(res, 400, 'Please submit a url with querystring')
   }
+  
+  if (!isUrl.test(url)) {
+    return send(res, 400, 'Please only submit http(s) urls')
+  }
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
+  try {
+    const result = await unfurl(url)
+    send(res, 200, result)
+  } catch (err) {
+    send(res, 500, err)
+  }
+
 }
 
 module.exports = router(
